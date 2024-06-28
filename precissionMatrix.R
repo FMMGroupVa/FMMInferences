9f09f95ae5422b8aa717c6f6aa345a76d3e13e20
FMMGroupVa <117477025+FMMGroupVa@users.noreply.github.com>
2024-06-28 10:01:25 +0200

    Add files via upload

--- precissionMatrix.R
+++ precissionMatrix.R
@@ -1,7 +1,7 @@
 
 require(magic)
 
-precissionMatrixFMM3d <- function(paramArrayArg, t){
+precissionMatrix3DFMMm <- function(paramArrayArg, t){
   d <- dim(paramArrayArg)[3]
   m <- dim(paramArrayArg)[1]
   
@@ -17,7 +17,6 @@ precissionMatrixFMM3d <- function(paramArrayArg, t){
   if(nrow(u)<ncol(v)){u <- t(u); v <- t(v)}
   
   # Derivatives dMu/dParam
-  
   blockM <- as.matrix(rep(1, length(t)*d))
   blockDelta <- u
   for (i in 1:(d-1)) {
@@ -45,3 +44,42 @@ precissionMatrixFMM3d <- function(paramArrayArg, t){
   F0 <- cbind(blockM, blockDelta, blockGamma, blockAlphas, blockOmegas)
   return(t(F0)%*%F0)
 }
+
+precissionMatrixFMM1 <- function(A, alpha, beta, omega, t){
+  delta <- A*cos(beta)
+  gamma <- -A*sin(beta)
+  tl <- 2*atan(omega*tan((t-alpha)/2))
+  u <- sin(tl)
+  v <- cos(tl)
+  derivM <- rep(1, length(t))
+  derivDelta <- v
+  derivGamma <- u
+  derivAlpha <- (omega + (1 - omega^2)*(1-v)/(2*omega))*(delta*u - gamma*v)
+  derivOmega <- u*(-delta*u + gamma*v)/omega
+  F0 <- cbind(derivM, derivDelta, derivGamma, derivAlpha, derivOmega)
+  return(t(F0)%*%F0)
+}
+
+precissionMatrixFMMm <- function(A, alpha, beta, omega, t){
+  
+  # Pre-calculus tl, u(alpha, omega, tl), (alpha, omega, tl)
+  m <- length(alpha)
+  delta <- A*cos(beta)
+  gamma <- -A*sin(beta)
+  tl <- apply(cbind(alpha, omega), 1, function(x){2*atan(x[2]*tan((t-x[1])/2))})
+  u <- t(apply(tl, 1, sin))
+  v <- t(apply(tl, 1, cos))
+  if(nrow(u)<ncol(v)){u <- t(u); v <- t(v)}
+  
+  # Derivatives dMu/dParam
+  derivM <- as.matrix(rep(1, length(t)))
+  derivDelta <- v
+  derivGamma <- u
+  
+  derivAlpha <- sapply(1:m, function(x){(omega[x] + (1 - omega[x]^2)*(1-v[,x])/(2*omega[x]))*(delta[x]*u[,x] - gamma[x]*v[,x])})
+  derivOmega <- sapply(1:m, function(x){u[,x]*(-delta[x]*u[,x] + gamma[x]*v[,x])/omega[x]})
+  
+  F0 <- cbind(derivM, derivDelta, derivGamma, derivAlpha, derivOmega)
+  return(t(F0)%*%F0)
+}
+
