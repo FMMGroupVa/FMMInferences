c10f23924dde09eb73ca81f66cdcdf8af340ba6f
FMMGroupVa <117477025+FMMGroupVa@users.noreply.github.com>
2024-09-30 13:38:50 +0200

    Update auxMultiFMM.R

--- auxMultiFMM.R
+++ auxMultiFMM.R
@@ -2,9 +2,10 @@
 require("RColorBrewer")
 require(R.utils)
 library(parallel)
-sourceDirectory("funcionesFMM/",modifiedOnly=F)
-source("auxMultiFMMPlot.R")
-source("precissionMatrix.R")
+dirfile <- dirname(rstudioapi::getSourceEditorContext()$path)
+sourceDirectory(paste0(dirfile, "/funcionesFMM/"),modifiedOnly=F)
+source(paste0(dirfile, "/auxMultiFMMPlot.R"))
+source(paste0(dirfile, "/precissionMatrix.R"))
 
 #### Fit of multiFMM model functions ####
 fitMultiFMM <- function(vDataMatrix, timePoints = seqTimes(nrow(vDataMatrix)), nBack = 5, maxIter = 10, weightError = TRUE,
